2665a8307b1ad73457534f4621d55bef4727d1eb
SpeedCodeWithMoksh <shahmoksh50@gmail.com>
2022-06-15 18:33:05 +0530

    Tutorial-3

---
+++ Tutorial-3/pages/blogpost/blog-1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const blog1 = () => {
+  return (
+    <div>blog-1</div>
+  )
+}
+
+export default blog1
---
+++ Tutorial-3/pages/contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>Contact.js</div>
+  )
+}
+
+export default contact
