b14b951dd4ed6c7c6c3b8a2843215817c5e48d1b
SpeedCodeWithMoksh <105773715+SpeedCodeWithMoksh@users.noreply.github.com>
2022-06-30 09:39:20 +0530

    Update getblog.js

--- NewsChaser Blog/pages/api/getblog.js
+++ NewsChaser Blog/pages/api/getblog.js
@@ -3,7 +3,7 @@
 import * as fs from 'node:fs';
 
 export default function handler(req, res) {
-  fs.readFile(`blogdata/${req.query.slug}.json`, (err, data)=>{
+  fs.readFile(`blogdata/${req.query.slug}.json`, 'utf-8', (err, data)=>{
     console.log(req.query.slug)
     res.status(200).json(JSON.parse(data))
   })
