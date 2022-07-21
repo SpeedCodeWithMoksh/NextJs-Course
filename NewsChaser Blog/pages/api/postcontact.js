495379456d8b165a7a644db0262c035584c7e75d
SpeedCodeWithMoksh <shahmoksh50@gmail.com>
2022-07-21 17:56:03 +0530

    Tutorial-20

--- NewsChaser Blog/pages/api/postcontact.js
+++ NewsChaser Blog/pages/api/postcontact.js
@@ -4,7 +4,6 @@ export default async function handler(req, res) {
     if (req.method === 'POST') {
       // Process a POST request
        let data = await fs.promises.readdir('contactdata');
-       console.log(data)
        fs.promises.writeFile(`contactdata/formdata-${data.length+1}.json`, JSON.stringify(req.body))
       res.status(200).json(req)
     } else {
--- NewsChaser Blog/pages/contact.js
+++ NewsChaser Blog/pages/contact.js
@@ -1,8 +1,51 @@
 import React from 'react'
+import Head from 'next/head'
 
 const contact = () => {
   return (
-    <div>contact</div>
+    <div>
+      <Head>
+        <title>Contact | NewsChaser</title>
+      </Head>
+      <section className="text-gray-600 body-font relative">
+  <div className="container px-5 py-12 mx-auto">
+    <div className="flex flex-col text-center w-full mb-12">
+      <h1 className="sm:text-3xl md:text-4xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
+    </div>
+    <div className="lg:w-1/2 md:w-2/3 mx-auto">
+      <div className="flex flex-wrap -m-2">
+        <div className="p-2 w-1/2">
+          <div className="relative">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
+            <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          </div>
+        </div>
+        <div className="p-2 w-1/2">
+          <div className="relative">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
+            <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          </div>
+        </div>
+        <div className="p-2 w-full">
+          <div className="relative">
+            <label htmlFor="phone" className="leading-7 text-sm text-gray-600">Phone Nunber</label>
+            <input type="phone" id="phone" name="phone" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          </div>
+        </div>
+        <div className="p-2 w-full">
+          <div className="relative">
+            <label htmlFor="message" className="leading-7 text-sm text-gray-600">Enter your Concern</label>
+            <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+          </div>
+        </div>
+        <div className="p-2 w-full">
+          <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    </div>
   )
 }
 
