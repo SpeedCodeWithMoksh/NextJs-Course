1a2c6cf0089166891956b7630e0f27a00f85c57d
SpeedCodeWithMoksh <shahmoksh50@gmail.com>
2022-08-04 17:15:53 +0530

    Tutorial-22

--- NewsChaser Blog/pages/about.js
+++ NewsChaser Blog/pages/about.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import Head from 'next/head'
 
 const about = () => {
   return (
-    <div>about</div>
+    <div>
+      <Head>
+        <title> About | Newschaser</title>
+        </Head>
+
+        <section className="text-gray-600 body-font">
+  <div className="container px-5 py-12 mx-auto flex flex-col">
+    <div className="lg:w-4/6 mx-auto">
+      <div className="rounded-lg h-64 overflow-hidden">
+        <img alt="content" className="object-cover object-top h-full w-full" src="hero.jpg" />
+      </div>
+      <div className="flex flex-col sm:flex-row mt-10">
+        <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+          <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+           <img className='rounded-full' src="user-img.jpg" alt="" />
+              <circle cx="12" cy="7" r="4"></circle>
+          </div>
+          <div className="flex flex-col items-center text-center justify-center">
+            <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">Phoebe Caulfield</h2>
+            <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
+            <p className="text-base">Raclette knausgaard hella meggs normcore williamsburg enamel pin sartorial venmo tbh hot chicken gentrify portland.</p>
+          </div>
+        </div>
+        <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+          <p className="leading-relaxed text-lg mb-4">Meggings portland fingerstache lyft, post-ironic fixie man bun banh mi umami everyday carry hexagon locavore direct trade art party. Locavore small batch listicle gastropub farm-to-table lumbersexual salvia messenger bag. Coloring book flannel truffaut craft beer drinking vinegar sartorial, disrupt fashion axe normcore meh butcher. Portland scenester vexillologist forage post-ironic asymmetrical, chartreuse disrupt butcher paleo intelligentsia pabst before they sold out four loko. 3 wolf moon brooklyn.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    </div>
   )
 }
 
